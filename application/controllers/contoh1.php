a8434fb8b0971ffac87e1b19ffd1bb81c4de6976
ADafaNK <awangdafaf@gmail.com>
2022-09-16 12:52:15 +0700

    menambahkan contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya <b>Awang Dafa Faturiansyah</b><br>
+            Saya tinggal di Desa Jeruju Besar<br>
+            hobi yang saya sukai salah satunya mengambar<br>
+            olah raga yang sering saya lakukan adalah<br>
+            Batminton/Bulutangkis dan juga Voli";
+    }
+}
