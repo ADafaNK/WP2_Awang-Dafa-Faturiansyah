8187e7a5f6664f423196bb7e06886b65deb1d067
ADafaNK <awangdafaf@gmail.com>
2022-09-26 15:50:51 +0700

    M

--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -1,7 +1,7 @@
 <html>
-    <head>
-        <title>latihan 1</title>
-    </head>
+<head>
+    <title>latihan 1</title>
+</head>
 <body>
     Halo Kawan.. Yuk kita belajar web programming..!!!<br>
     Nilai 1 = <?= $nilai1; ?>
