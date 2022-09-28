69826591c45f0b9a2f1405af72c2364997ad2628
ADafaNK <awangdafaf@gmail.com>
2022-09-28 13:55:55 +0700

    tugas 3 ( '' v '' )

---
+++ application/views/v_about.php
@@ -0,0 +1,27 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Awang Dafa</li>
+        <li>Nama Belakang : Faturiansyah</li>
+    </ul>
+    
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jeruju Besar</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Mataram</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Voli</li>
+        <li>Catur</li>
+        <li>Tenis Meja</li>
+        <li>Kasti</li>
+    </ul>
+</section>
