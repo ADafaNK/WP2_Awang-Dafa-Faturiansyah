a52683eb9fdeaeaf8aed58e550d9239089dee464
ADafaNK <awangdafaf@gmail.com>
2022-10-22 22:52:46 +0700

    tugas 6 ( X 3 X )

---
+++ application/views/view-data-siswa.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+    <title>Tampil Data Matakuliah</title>
+</head>
+<body>
+    <center>
+        <table>
+            <tr>
+                <th colspan="3">
+                    Tampil Data Siswa
+                </th>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <hr>
+                </td>
+            </tr>
+            <tr>
+                <th>Nama Siswa</th>
+                <th>:</th>
+                <td>
+                    <?= $nama; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>NIS</td>
+                <td>:</td>
+                <td>
+                    <?= $nis; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Kelas</td>
+                <td>:</td>
+                <td>
+                    <?= $kelas; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Tanggal Lahir</td>
+                <td>:</td>
+                <td>
+                    <?= $tanggal; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>tempat Lahir</td>
+                <td>:</td>
+                <td>
+                    <?= $tempat; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td>
+                    <?= $alamat; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Jenis kelamin</td>
+                <td>:</td>
+                <td>
+                    <?= $jenis; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Agama</td>
+                <td>:</td>
+                <td>
+                    <?= $agama; ?>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3" align="center">
+                    <a href="<?= base_url('datasiswa'); ?>">Kembali</a>
+                </td>
+            </tr>
+        </table>
+    </center>
+</body>
+</html>
